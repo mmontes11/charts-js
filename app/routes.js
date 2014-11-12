814294a0c5ad4aba5932eba42caafdcfe17efca5
Martin Montes <martin11lrx@gmail.com>
2014-11-12 11:09:35 +0100

    Reesctructured and configured project

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-var Todo = require('./models/todo');
+var Todo = require('../db/models/todo');
 
 module.exports = function(app) {
 
@@ -57,6 +57,7 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		// load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/index.html'); 
 	});
 };
