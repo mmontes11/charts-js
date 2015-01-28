fd16dc7e3c74279e07d7fd56b13698fe847f7755
unknown <mmontes@ITG0120.itgnet.itg.es>
2015-01-28 13:35:32 +0100

    changed architecture

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var methodOverride = require('method-override');
 // configuration ===============================================================
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/client')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
--- server/routes.js
+++ server/routes.js
@@ -77,6 +77,6 @@ module.exports = function(app) {
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
 		// load the single view file (angular will handle the page changes on the front-end)
-		res.sendfile('./public/index.html'); 
+		res.sendfile('./client/index.html'); 
 	});
 };
